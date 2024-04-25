94de0a2b5e4a71d2afdfad63255e6381a41f38e3
kaikeventura <kaike.gnr@hotmail.com>
2024-04-25 00:38:26 -0300

    Incluso tabs para separação

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
 import {SignoutComponent} from "../signout/signout.component";
 import {InvoiceComponent} from "./invoice/invoice.component";
+import {MatTab, MatTabGroup, MatTabLabel} from "@angular/material/tabs";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +11,11 @@ import {InvoiceComponent} from "./invoice/invoice.component";
   imports: [
     MatToolbar,
     SignoutComponent,
-    InvoiceComponent
+    InvoiceComponent,
+    MatTabGroup,
+    MatTab,
+    MatIcon,
+    MatTabLabel
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
